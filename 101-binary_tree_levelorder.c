b0016a333db93e4ac0eccdf179516360108b6b8f
soabolaji <soabolaji@gmail.com>
2023-03-29 10:21:41 +0100

    Binary trees size

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,16 +1,5 @@
 #include "binary_trees.h"
 
-/**
- * struct node_s - singly linked list
- * @node: const binary tree node
- * @next: points to the next node
- */
-typedef struct node_s
-{
-	const binary_tree_t *node;
-	struct node_s *next;
-} ll;
-
 ll *append(ll *head, const binary_tree_t *btnode);
 void free_list(ll *head);
 ll *get_children(ll *head, const binary_tree_t *parent);
@@ -18,7 +7,7 @@ void levels_rec(ll *head, void (*func)(int));
 
 /**
  * binary_tree_levelorder - Goes through a binary tree
- *                          using level-order traversal.
+ *			using level-order traversal.
  * @tree: Pointer to the root node of the tree to traverse.
  * @func: Pointer to a function to call for each node.
  */
--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,17 @@ typedef struct levelorder_queue_s
 	struct levelorder_queue_s *next;
 } levelorder_queue_t;
 
+/**
+ * struct node_s - singly linked list
+ * @node: const binary tree node
+ * @next: points to the next node
+ */
+typedef struct node_s
+{
+	const binary_tree_t *node;
+	struct node_s *next;
+} ll;
+
 /* Printing helper function */
 void binary_tree_print(const binary_tree_t *);
 
